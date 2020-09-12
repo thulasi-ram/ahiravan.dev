8bd25d4bdf26784112532898bfcf8c1f8c18e5cc
Thulasi Ram <thulasi503@gmail.com>
2020-09-12 18:29:15 +0530

    Cosmetic changes to logo and fixed blog not rendering

--- src/components/posts.js
+++ src/components/posts.js
@@ -6,38 +6,41 @@ import {Styled, css} from "theme-ui"
 import {Link} from "gatsby"
 // import PostList from "gatsby-theme-blog/src/components/post-list"
 
-const Posts = ({ location, posts, siteTitle, socialLinks }) => (
-  <Layout location={location} title={siteTitle}>
-    <SEO title="Home" />
-    {posts.map(({ node }) => {
-        const title = node.title || node.slug
-        const keywords = node.keywords || []
-        return (
-          <Fragment key={node.slug}>
-            <SEO title="Home" keywords={keywords} />
-            <div>
-              <Styled.h2
-                css={css({
-                  mb: 1,
-                })}
-              >
-                <Styled.a
-                  as={Link}
-                  css={{
-                    textDecoration: `none`,
-                  }}
-                  to={node.slug}
+const Posts = ({ location, posts, siteTitle, socialLinks }) =>{
+  console.log("koko", posts)
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="Home" />
+      {posts.map((node) => {
+          const title = node.title || node.slug
+          const keywords = node.keywords || []
+          return (
+            <Fragment key={node.slug}>
+              <SEO title="Home" keywords={keywords} />
+              <div>
+                <Styled.h2
+                  css={css({
+                    mb: 1,
+                  })}
                 >
-                  {title}
-                </Styled.a>
-              </Styled.h2>
-              <small>{node.date}</small>
-              <Styled.p>{node.excerpt}</Styled.p>
-            </div>
-          </Fragment>
-        )
-      })}
-  </Layout>
-)
+                  <Styled.a
+                    as={Link}
+                    css={{
+                      textDecoration: `none`,
+                    }}
+                    to={node.slug}
+                  >
+                    {title}
+                  </Styled.a>
+                </Styled.h2>
+                <small>{node.date}</small>
+                <Styled.p>{node.excerpt}</Styled.p>
+              </div>
+            </Fragment>
+          )
+        })}
+    </Layout>
+  )
+} 
 
 export default Posts
--- src/gatsby-theme-blog-core/components/post.js
+++ src/gatsby-theme-blog-core/components/post.js
@@ -3,7 +3,7 @@ import Post from "../../components/post"
 
 export default ({ location, data }) => {
   const { blogPost, previous, next } = data
-
+  console.log("yoyo", data)
   return (
     <Post
       data={{ ...data, post: blogPost }}
