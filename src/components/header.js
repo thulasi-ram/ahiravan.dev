c1de739af0ad074a6862d1d75dd00cd6f3d15366
Thulasi Ram <thulasi503@gmail.com>
2020-09-14 22:47:30 +0530

    Corrected sun svg

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import { Flex, IconButton, jsx, Styled, useColorMode } from "theme-ui"
 import LogoSvg from "../svgs/logo.svg"
+import SunSvg from "../svgs/sun.svg"
 
 const navLink = { p: 0, mx: 3, textTransform: "lowercase", lineHeight: 1 }
 
@@ -59,23 +60,7 @@ const Header = () => {
             setColorMode(colorMode === "default" ? "dark" : "default")
           }}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            width="18"
-            height="18"
-            fill="currentcolor"
-          >
-            <circle
-              r={9}
-              cx={10}
-              cy={10}
-              fill="none"
-              stroke="currentcolor"
-              strokeWidth={2}
-            />
-            <path d="M 10 0 A 10 10 0 0 0 10 20 z"></path>
-          </svg>
+          <SunSvg></SunSvg>
         </IconButton>
       </Flex>
     </header>
