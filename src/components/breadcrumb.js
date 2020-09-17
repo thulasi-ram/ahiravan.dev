c636b776045b803077158119d38f5645ad193acb
Thulasi Ram <thulasi503@gmail.com>
2020-09-17 22:49:47 +0530

    content fixes and cleanup

--- src/components/breadcrumb.js
+++ src/components/breadcrumb.js
@@ -2,42 +2,49 @@
 import { Link } from "gatsby"
 import { jsx, Styled } from "theme-ui"
 
+const crumbLinkSx = (active) => {
+  const sx = {
+    color: 'secondary'
+  }
+  if (!active) {
+    sx['textDecoration']= "none"
+  }
+  return (sx)
+}
+
 const BreadCrumb = ({ crumbs }) => {
-
   return (
-
-      <nav aria-label="Breadcrumb">
-        <ol
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            listStyle: "none",
-            margin: 0,
-            pl: 0,
-          }}
-        >
-          {crumbs.map((c, i) => {
-            return (
-              <li key={"crmb"  + i}>
-                <Styled.a
+    <nav aria-label="Breadcrumb">
+      <ol
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          listStyle: "none",
+          margin: 0,
+          pl: 0,
+        }}
+      >
+        {crumbs.map((c, i) => {
+          return (
+            <li key={"crmb" + i}>
+              <Styled.a
                 as={Link}
-                  to={c.pathname || ""}
-                  activeClassName="breadcrumb__link__active"
-                  aria-current={i === crumbs.length - 1 ? "page" : null}
-                >
-                  {c.crumbLabel}
-                </Styled.a>
-                {i === crumbs.length - 1 ? null : (
-                  <span aria-hidden="true" sx={{mx: 2}}>
+                to={c.pathname || ""}
+                aria-current={i === crumbs.length - 1 ? "page" : null}
+                sx={crumbLinkSx(i===crumbs.length - 1)}
+              >
+                {c.crumbLabel}
+              </Styled.a>
+              {i === crumbs.length - 1 ? null : (
+                <span aria-hidden="true" sx={{ mx: 2 }}>
                   {c.crumbSeparator}
-                  </span>
-                )}
-              </li>
-            )
-          })}
-        </ol>
-      </nav>
-
+                </span>
+              )}
+            </li>
+          )
+        })}
+      </ol>
+    </nav>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -19,8 +19,8 @@ const IndexPage = () => (
           <h1>Hi, I'm Thulasi</h1>
           <Heading>I build for all things web.</Heading>
 
-          <Grid columns={[1]} sx={{m: 4}}>
-            <Button> RESUME </Button>
+          <Grid columns={[1]} sx={{ m: 4 }}>
+            <Button variant="basic"> RESUME &#x2b07; </Button>
           </Grid>
         </Box>
       </Grid>
@@ -34,21 +34,38 @@ const IndexPage = () => (
           alignItems: "center",
         }}
       >
-        <Image/>
-        <p sx={{ mt: 0, textTransform: "uppercase", color: 'accent', fontSize: 2}}>
-          Web Developer | Designer{" "}
+        <Image />
+        <p
+          sx={{
+            mt: 0,
+            textTransform: "uppercase",
+            color: "accent",
+            fontSize: 2,
+          }}
+        >
+          backend Developer | Designer{" "}
         </p>
         <p> me{"{at}"}ahiravan.dev</p>
-        <Grid columns={[3]} 
-        sx={{          
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
+        <Grid
+          columns={[3]}
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
           }}
-          >
-        <Styled.a href="/" sx={{fontWeight: 'bold', textDecoration: "none", p: 1}}> GITHUB </Styled.a>
-        <Styled.a href="/" sx={{fontWeight: 'bold', textDecoration: "none", p: 1}}> TWITTER </Styled.a>
-        <Styled.a href="/" sx={{fontWeight: 'bold', textDecoration: "none", p: 1}}> LINKEDIN </Styled.a>
+        >
+          <Styled.a href="/" sx={{ fontWeight: "bold", p: 1 }}>
+            {" "}
+            GITHUB{" "}
+          </Styled.a>
+          <Styled.a href="/" sx={{ fontWeight: "bold", p: 1 }}>
+            {" "}
+            TWITTER{" "}
+          </Styled.a>
+          <Styled.a href="/" sx={{ fontWeight: "bold", p: 1 }}>
+            {" "}
+            LINKEDIN{" "}
+          </Styled.a>
         </Grid>
       </Card>
     </Grid>
@@ -57,23 +74,35 @@ const IndexPage = () => (
       <Flex>
         <Heading variant="section"> ABOUT ME </Heading>
       </Flex>
-      Building startups and Crafting software, currently based in Bengaluru,
-      India. Intrigued by design, armchair philosophy and anything CS. I also
-      like to play chess, swim, surf the web, read books and listen to music.
-      Seeking to be inspired and fueled by a passion for improving the quality
-      of life of the people through the products I design and create. I come
-      with a hybrid background in both backend systems and frontend
-      technologies. Polyglot. Have extensive hands on experience in Python, Go,
-      Javascript, Java and misc css frameworks. Have briefly tried out Clojure,
-      Haskell, Ruby and Elixir.
+      <p>Crafting software, with &#10084; from @simpl in Bengaluru, India. </p>
+
+      <p>
+        Intrigued by design, armchair philosophy and anything CS. Fueled by a
+        passion for improving the QOL through the products I design and create.
+      </p>
+
+      <p>
+        I like to play chess, swim, surf the web, read books and listen to music
+        in leisure.
+      </p>
     </section>
 
     <section>
       <Flex>
         <Heading variant="section"> WORK </Heading>
       </Flex>
-      Previously: tReebo, infosys currently: simpl oss contributions: Feature
-      Toggle - Python Diff and Patch - Python IvoryCRM (discontinued) - Python
+      <p>Previously at: Treebo, Infosys</p>
+      <p>
+        I come with a hybrid background in both backend systems and frontend
+        technologies. Polyglot. Have extensive hands on experience in Python,
+        Go, Javascript, Java and misc css frameworks. Have briefly tried out
+        Clojure, Haskell, Ruby and Elixir.
+      </p>
+
+      <h4>oss contributions: </h4>
+      <p>Feature Toggle - Python </p>
+      <p>Diff and Patch - Python </p>
+      <p>IvoryCRM (discontinued) - Python</p>
     </section>
   </div>
 )
