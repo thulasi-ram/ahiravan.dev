62f1bc82fed4b3b3507bed991313cdf3d9daaf9f
Thulasi Ram <thulasi503@gmail.com>
2020-09-21 22:36:49 +0530

    Font optimizations based on
    https://www.zachleat.com/web/comprehensive-webfonts/
    https://markoskon.com/creating-font-subsets/

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,32 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.onClientEntry = () => {
+    console.log("We've started!")
+    if( "fonts" in document ) {
+        // Optimization for Repeat Views
+        if( sessionStorage.fontsLoadedCriticalFoftPreload ) {
+            console.log("ls load")
+            document.documentElement.className += " fonts-loaded-2";
+            return;
+        }
+
+        document.fonts.load("1em LatoSubset").then(function () {
+            document.documentElement.className += " fonts-loaded-1";
+
+            Promise.all([
+                document.fonts.load("400 1em Lato"),
+                document.fonts.load("700 1em Lato"),
+                document.fonts.load("italic 1em Lato"),
+                document.fonts.load("italic 700 1em Lato")
+            ]).then(function () {
+                document.documentElement.className += " fonts-loaded-2";
+                console.log("pr set")
+
+                // Optimization for Repeat Views
+                sessionStorage.fontsLoadedCriticalFoftPreload = true;
+            });
+        });
+    }
+}
