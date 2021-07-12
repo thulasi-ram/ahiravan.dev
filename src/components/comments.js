d8133d2baff12ca05ec735a2f790b84f6f901b6a
Thulasi Ram <thulasi503@gmail.com>
2021-07-12 20:31:15 +0530

    fix: access browser side env with GATSBY_ prefix

--- src/components/comments.js
+++ src/components/comments.js
@@ -2,33 +2,6 @@
 import React, {useEffect} from "react"
 import Helmet from "react-helmet"
 import { Box, Flex, Heading, jsx } from "theme-ui"
-// class CactusComment extends React.Component {
-//   componentDidMount() {
-//     let cactusOptions = {
-//       node: document.getElementById("comment-section"),
-//       defaultHomeserverUrl: "https://matrix.cactus.chat:8448",
-//       serverName: "cactus.chat",
-//       siteName: process.env.CACTUS_CHAT_SITE_NAME,
-//       commentSectionId: `${process.env.NODE_ENV}-${this.props.path}`
-//     }
-//     console.log(`${cactusOptions.commentSectionId}`)
-
-//     if (window.initComments) {
-//       window.initComments(cactusOptions) 
-//     } else {
-//       console.log("initComments is undefined, retrying in 3 sec")
-//       setTimeout(function(){
-//         window.initComments(cactusOptions) 
-//        }, 3000);
-//     }
-//   }
-
-//   render() {
-//       return (
-//         <div id="comment-section"></div>
-//       );
-//   }
-//   }
 
 
 const CactusComment = ({path}) => {
@@ -36,11 +9,12 @@ const CactusComment = ({path}) => {
   let commentID = `${process.env.NODE_ENV}-${path}`;
 
   useEffect( () => {
+
       let cactusOptions = {
         node: document.getElementById("comment-section"),
         defaultHomeserverUrl: "https://matrix.cactus.chat:8448",
         serverName: "cactus.chat",
-        siteName: process.env.CACTUS_CHAT_SITE_NAME,
+        siteName: process.env.GATSBY_CACTUS_CHAT_SITE_NAME,
         commentSectionId: commentID
     }
     console.log(`${cactusOptions.commentSectionId}`)
