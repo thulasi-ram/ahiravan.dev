d7beef11ba5dfe5fd38fea6dcd85d2458bc4e4d4
Thulasi Ram <thulasi503@gmail.com>
2021-07-12 18:24:14 +0530

    feat: added cactus chat

--- src/components/comments.js
+++ src/components/comments.js
@@ -1,45 +1,28 @@
 /** @jsx jsx */
-import Valine from "gatsby-plugin-valine"
+import React from "react"
+import Helmet from "react-helmet"
 import { Box, Flex, Heading, jsx } from "theme-ui"
-var langMode = {
-  nick: "NAme",
-  head: {
-    nick: "NAme",
-    mail: "E-Mail",
-    link: "Website(http://)",
-  },
-  tips: {
-    comments: "Comments",
-    sofa: "No comments yet.",
-    busy: "Submit is busy, please wait...",
-    again: "Sorry, this is a wrong calculation.",
-  },
-  ctrl: {
-    reply: "Reply",
-    ok: "Ok",
-    sure: "Sure",
-    cancel: "Cancel",
-    confirm: "Confirm",
-    continue: "Continue",
-    more: "Load More...",
-    try: "Once More?",
-    preview: "Preview",
-    emoji: "Emoji",
-  },
-  error: {
-    99: "Initialization failed, Please check the `el` element in the init method.",
-    100: "Initialization failed, Please check your appId and appKey.",
-    401: "Unauthorized operation, Please check your appId and appKey.",
-    403: "Access denied by api domain white list, Please check your security domain.",
-  },
-  timeago: {
-    seconds: "seconds ago",
-    minutes: "minutes ago",
-    hours: "hours ago",
-    days: "days ago",
-    now: "just now",
-  },
-}
+class CactusComment extends React.Component {
+  componentDidMount() {
+    if (window.initComments) {
+      window.initComments({
+        node: document.getElementById("comment-section"),
+        defaultHomeserverUrl: "https://matrix.cactus.chat:8448",
+        serverName: "cactus.chat",
+        siteName: "ahiravan.dev",
+        commentSectionId: this.props.path
+      }) 
+    } else {
+      console.log("initComments is undefined")
+    }
+  }
+
+  render() {
+      return (
+        <div id="comment-section"></div>
+      );
+  }
+  }
 
 export const CommentsComponent = ({current_url}) => {
   return (
@@ -50,7 +33,27 @@ export const CommentsComponent = ({current_url}) => {
           COMMENTS{" "}
         </Heading>
       </Flex>
-      <Valine path={current_url}/>
+      <CactusComment path={current_url}/>
     </Box>
+  )
+}
+
+const options = {
+  node: document.getElementById("comment-section"),
+  defaultHomeserverUrl: "https://matrix.cactus.chat:8448",
+  serverName: "cactus.chat",
+  siteName: "ahiravan.dev",
+  commentSectionId: "section1"
+}
+
+export const CommentsHeader = () => {
+
+  return (
+    <React.Fragment>
+      <Helmet>
+      <script type="text/javascript" src="https://latest.cactus.chat/cactus.js"></script>
+      <link rel="stylesheet" href="https://latest.cactus.chat/style.css" type="text/css"></link>
+      </Helmet>
+    </React.Fragment>
   )
 }
