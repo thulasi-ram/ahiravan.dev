4239bfcf9f91a404e020c2b1ddd1beccc84361d4
Thulasi Ram <thulasi503@gmail.com>
2021-07-12 19:16:31 +0530

    fix: move from componentDidMount to useEffect since gatsby was not able to access document

--- src/components/comments.js
+++ src/components/comments.js
@@ -1,34 +1,65 @@
 /** @jsx jsx */
-import React from "react"
+import React, {useEffect} from "react"
 import Helmet from "react-helmet"
 import { Box, Flex, Heading, jsx } from "theme-ui"
-class CactusComment extends React.Component {
-  componentDidMount() {
-    let cactusOptions = {
-      node: document.getElementById("comment-section"),
-      defaultHomeserverUrl: "https://matrix.cactus.chat:8448",
-      serverName: "cactus.chat",
-      siteName: process.env.CACTUS_CHAT_SITE_NAME,
-      commentSectionId: `${process.env.NODE_ENV}-${this.props.path}`
+// class CactusComment extends React.Component {
+//   componentDidMount() {
+//     let cactusOptions = {
+//       node: document.getElementById("comment-section"),
+//       defaultHomeserverUrl: "https://matrix.cactus.chat:8448",
+//       serverName: "cactus.chat",
+//       siteName: process.env.CACTUS_CHAT_SITE_NAME,
+//       commentSectionId: `${process.env.NODE_ENV}-${this.props.path}`
+//     }
+//     console.log(`${cactusOptions.commentSectionId}`)
+
+//     if (window.initComments) {
+//       window.initComments(cactusOptions) 
+//     } else {
+//       console.log("initComments is undefined, retrying in 3 sec")
+//       setTimeout(function(){
+//         window.initComments(cactusOptions) 
+//        }, 3000);
+//     }
+//   }
+
+//   render() {
+//       return (
+//         <div id="comment-section"></div>
+//       );
+//   }
+//   }
+
+
+const CactusComment = ({path}) => {
+
+  let commentID = `${process.env.NODE_ENV}-${path}`;
+
+  useEffect( () => {
+      let cactusOptions = {
+        node: document.getElementById("comment-section"),
+        defaultHomeserverUrl: "https://matrix.cactus.chat:8448",
+        serverName: "cactus.chat",
+        siteName: process.env.CACTUS_CHAT_SITE_NAME,
+        commentSectionId: commentID
     }
     console.log(`${cactusOptions.commentSectionId}`)
-
+    
+    let timeout = 3000;
     if (window.initComments) {
-      window.initComments(cactusOptions) 
-    } else {
-      console.log("initComments is undefined, retrying in 3 sec")
-      setTimeout(function(){
-        window.initComments(cactusOptions) 
-       }, 3000);
+      // since it takes sometime for cactus to load
+      timeout = 0;
     }
-  }
+    setTimeout(function(){
+      window.initComments(cactusOptions) 
+      }, timeout);
+   }, [ commentID ] );
 
-  render() {
-      return (
-        <div id="comment-section"></div>
-      );
-  }
-  }
+
+  return (
+    <div id="comment-section"></div>
+  )
+}
 
 export const CommentsComponent = ({current_url}) => {
   return (
@@ -42,14 +73,6 @@ export const CommentsComponent = ({current_url}) => {
       <CactusComment path={current_url}/>
     </Box>
   )
-}
-
-const options = {
-  node: document.getElementById("comment-section"),
-  defaultHomeserverUrl: "https://matrix.cactus.chat:8448",
-  serverName: "cactus.chat",
-  siteName: "ahiravan.dev",
-  commentSectionId: "section1"
 }
 
 export const CommentsHeader = () => {
