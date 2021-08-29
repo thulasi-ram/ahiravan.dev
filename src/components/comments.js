7620b60aae8bcd9a3cac11a566e31bc3e7db06f0
Thulasi Ram <thulasi503@gmail.com>
2021-08-29 20:05:19 +0530

    fix: lazy load comments

--- src/components/comments.js
+++ src/components/comments.js
@@ -1,41 +1,63 @@
 /** @jsx jsx */
-import React, {useEffect} from "react"
+import React, { useEffect } from "react"
 import Helmet from "react-helmet"
 import { Box, Flex, Heading, jsx } from "theme-ui"
 
-
-const CactusComment = ({path}) => {
-
-  let commentID = `${process.env.NODE_ENV}-${path}`;
-
-  useEffect( () => {
-
-      let cactusOptions = {
-        node: document.getElementById("comment-section"),
-        defaultHomeserverUrl: "https://matrix.cactus.chat:8448",
-        serverName: "cactus.chat",
-        siteName: process.env.GATSBY_CACTUS_CHAT_SITE_NAME,
-        commentSectionId: commentID
+const loadCactusScript = callback => {
+  const existingScript = window.initComments
+  if (!existingScript) {
+    const script = document.createElement("script")
+    script.src = "https://latest.cactus.chat/cactus.js"
+    script.id = "cactusComments"
+    document.body.appendChild(script)
+    script.onload = () => {
+      if (callback) callback()
     }
-    console.log(`${cactusOptions.commentSectionId}`)
-    
-    let timeout = 3000;
-    if (window.initComments) {
-      // since it takes sometime for cactus to load
-      timeout = 0;
-    }
-    setTimeout(function(){
-      window.initComments(cactusOptions) 
-      }, timeout);
-   }, [ commentID ] );
+  }
+  if (existingScript && callback) callback()
+}
 
+const CactusComment = ({ path }) => {
+  let commentID = `${process.env.NODE_ENV}-${path}`
+
+  const cactusOptions = {
+    node: null,
+    defaultHomeserverUrl: "https://matrix.cactus.chat:8448",
+    serverName: "cactus.chat",
+    siteName: process.env.GATSBY_CACTUS_CHAT_SITE_NAME,
+    commentSectionId: commentID,
+  }
+
+  useEffect(() => {
+    loadCactusScript(() => {
+      cactusOptions.node = document.getElementById("comment-section")
+      window.initComments(cactusOptions)
+    })
+  }, [cactusOptions])
+  // why to return cactusOptions here
+  // ref: https://stackoverflow.com/questions/61956823/why-cant-useeffect-access-my-state-variable-in-a-return-statement
 
   return (
-    <div id="comment-section"></div>
+    <React.Fragment>
+      <Helmet>
+        <link
+          rel="preload"
+          as="style"
+          href="https://latest.cactus.chat/style.css"
+          onload="this.onload=null;this.rel='stylesheet'"
+        ></link>
+        <noscript>
+          {`<link rel="stylesheet" href="https://latest.cactus.chat/style.css"/>`}
+        </noscript>
+        {/* <script type="text/javascript" defer src="https://latest.cactus.chat/cactus.js"   onLoad={`console.log('HAAEHHHAHHEHHHA')`} */}
+        {/* ></script> */}
+      </Helmet>
+      <div id="comment-section"></div>
+    </React.Fragment>
   )
 }
 
-export const CommentsComponent = ({current_url}) => {
+export const CommentsComponent = ({ current_url }) => {
   return (
     <Box>
       <Flex>
@@ -44,19 +66,7 @@ export const CommentsComponent = ({current_url}) => {
           COMMENTS{" "}
         </Heading>
       </Flex>
-      <CactusComment path={current_url}/>
+      <CactusComment path={current_url} />
     </Box>
-  )
-}
-
-export const CommentsHeader = () => {
-
-  return (
-    <React.Fragment>
-      <Helmet>
-      <script type="text/javascript" src="https://latest.cactus.chat/cactus.js"></script>
-      <link rel="stylesheet" href="https://latest.cactus.chat/style.css" type="text/css"></link>
-      </Helmet>
-    </React.Fragment>
   )
 }
