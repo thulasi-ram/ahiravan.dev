58b65930cdcd025d75968070c541c73f6b90a775
Thulasi Ram <thulasi503@gmail.com>
2021-09-03 14:44:46 +0530

    fix: moved to yarn

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,8 +48,20 @@ module.exports = {
       },
     },
     // `gatsby-theme-style-guide`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Defaults used for gatsbyImageData and StaticImage
+        defaults: {},
+        failOnError: true,
+        base64Width: 20,
+        forceBase64Format: `webp`, // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
+      },
+    },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
