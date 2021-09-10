d717040bf5b534bbdca2fd5f299f4f69323f344f
Thulasi Ram <thulasi503@gmail.com>
2021-09-10 21:38:43 +0530

    fix: added sentry integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -166,5 +166,11 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: "@sentry/gatsby",
+      options: {
+        dsn: process.env.SENTRY_DSN,
+      },
+    },
   ],
 }
