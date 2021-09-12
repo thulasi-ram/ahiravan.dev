9b8a7e61b7894fb99ccb3fda892ff8cf3967a462
Thulasi Ram <thulasi503@gmail.com>
2021-09-12 15:14:16 +0530

    fix: make sentry lazy

--- src/components/sentry.js
+++ src/components/sentry.js
@@ -12,7 +12,7 @@ const SentryHeader = () => {
       <script
         src="https://js.sentry-cdn.com/4af2c68a592849b58c97e46d52933c09.min.js"
         crossorigin="anonymous"
-        data-lazy="no" // https://docs.sentry.io/platforms/javascript/install/lazy-load-sentry/#load-timing
+        // data-lazy="no" // https://docs.sentry.io/platforms/javascript/install/lazy-load-sentry/#load-timing
       ></script>
     </Helmet>
   )
