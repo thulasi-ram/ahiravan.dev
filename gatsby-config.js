4d66c30b4e63ea31bef6f502f4ca822be5478902
Thulasi Ram <thulasi503@gmail.com>
2021-09-12 15:49:58 +0530

    fix: conditionall enable bundle analyser

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,163 @@ console.log(`Using environment config: '${activeEnv}'`)
 require("dotenv").config({
   path: `.env.${activeEnv}`,
 })
+
+
+let plugins = [
+  `gatsby-plugin-preact`,
+  {
+    resolve: `gatsby-plugin-theme-ui`,
+    options: {
+      // prismPreset: `night-owl`,
+    },
+  },
+  `gatsby-plugin-react-helmet`,
+  {
+    resolve: `gatsby-theme-blog-core`,
+    options: {
+      basePath: `/blog`,
+    },
+  },
+  {
+    resolve: "gatsby-plugin-react-svg",
+    options: {
+      rule: {
+        include: `${__dirname}/src/svgs/`,
+      },
+    },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `images`,
+      path: `${__dirname}/src/images`,
+    },
+  },
+  {
+    resolve: `gatsby-plugin-sharp`,
+    options: {
+      defaults: {},
+      failOnError: true,
+      base64Width: 20,
+      forceBase64Format: `webp`,
+      backgroundColor: `transparent`,
+
+      stripMetadata: true,
+      defaultQuality: 50,
+    },
+  },
+  `gatsby-transformer-sharp`,
+  `gatsby-plugin-image`,
+  {
+    resolve: `gatsby-plugin-mdx`,
+    options: {
+      extensions: [`.md`, `.mdx`],
+    },
+  },
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `Ahiravan's Portfolio`,
+      short_name: `Ahiravan home and blog`,
+      start_url: `/`,
+      background_color: `#fff`,
+      theme_color: `#000`,
+      display: `standalone`,
+      lang: `en`,
+      icon: `src/images/favicon.png`,
+      icon_options: {
+        purpose: `any maskable`,
+      },
+    },
+  },
+  // {
+  //   resolve: "gatsby-plugin-transition-link",
+  //   options: {
+  //     layout: require.resolve(`./src/components/layout.js`),
+  //   },
+  // },
+  // {
+  //   resolve: `gatsby-plugin-lodash`,
+  //   options: {},
+  // },
+  "gatsby-plugin-robots-txt",
+  "gatsby-plugin-sitemap",
+  // "gatsby-plugin-emotion",
+  {
+    resolve: `gatsby-plugin-s3`,
+    options: {
+      bucketName: "ahiravan.dev",
+      acl: null,
+      protocol: "https",
+      hostname: "www.ahiravan.dev",
+    },
+  },
+  {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: process.env.GA_TRACKING_ID,
+    },
+  },
+  {
+    resolve: "gatsby-plugin-breakpoints",
+    options: {
+      queries: {
+        xs: "(max-width: 320px)",
+        sm: "(max-width: 720px)",
+        md: "(max-width: 1024px)",
+        l: "(max-width: 1536px)",
+        portrait: "(orientation: portrait)",
+        posts_list_break: "(max-width: 550px)",
+      },
+    },
+  },
+  {
+    resolve: "gatsby-omni-font-loader",
+    options: {
+      // mode: "render-blocking",
+      // enableListener: false,
+      custom: [
+        // {
+        //   name: ["Iosevka Aile Web"],
+        //   file: "/fonts/webfont-iosevka-aile-10.1.1/iosevka-aile.css",
+        // },
+        // {
+        //   name: ["Iosevka Etoile Web"],
+        //   file: "/fonts/webfont-iosevka-etoile-10.1.1/iosevka-etoile.css",
+        // },
+        {
+          name: ["Iosevka Aile Optimized", "Iosevka Etoile Optimized"],
+          file: "/fonts/iosevka-optimized-10.1.1/font.min.css",
+        }
+      ],
+    },
+  },
+  {
+    // why this: https://github.com/jariz/gatsby-plugin-s3/blob/master/recipes/with-cloudfront.md
+    resolve: `gatsby-plugin-canonical-urls`,
+    options: {
+      siteUrl: "https://www.ahiravan.dev",
+    },
+  },
+  "gatsby-plugin-minify-html",
+  "gatsby-plugin-brotli",
+  // ,
+  // this (optional) plugin enables Progressive Web App + Offline functionality
+  // To learn more, visit: https://gatsby.dev/offline
+  // `gatsby-plugin-offline`,
+  // {
+  //   resolve: "@sentry/gatsby",
+  //   options: {
+  //     dsn: process.env.SENTRY_DSN
+  //   },
+  // },
+]
+
+if (activeEnv == "development") {
+  plugins.push( "gatsby-plugin-webpack-bundle-analyser-v2")
+}
+
+
 module.exports = {
   siteMetadata: {
     title: `Ahiravan's Home`,
@@ -22,154 +179,5 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    `gatsby-plugin-preact`,
-    {
-      resolve: `gatsby-plugin-theme-ui`,
-      options: {
-        // prismPreset: `night-owl`,
-      },
-    },
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-theme-blog-core`,
-      options: {
-        basePath: `/blog`,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: `${__dirname}/src/svgs/`,
-        },
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-sharp`,
-      options: {
-        defaults: {},
-        failOnError: true,
-        base64Width: 20,
-        forceBase64Format: `webp`,
-        backgroundColor: `transparent`,
-
-        stripMetadata: true,
-        defaultQuality: 50,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        extensions: [`.md`, `.mdx`],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Ahiravan's Portfolio`,
-        short_name: `Ahiravan home and blog`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#000`,
-        display: `standalone`,
-        lang: `en`,
-        icon: `src/images/favicon.png`,
-        icon_options: {
-          purpose: `any maskable`,
-        },
-      },
-    },
-    // {
-    //   resolve: "gatsby-plugin-transition-link",
-    //   options: {
-    //     layout: require.resolve(`./src/components/layout.js`),
-    //   },
-    // },
-    // {
-    //   resolve: `gatsby-plugin-lodash`,
-    //   options: {},
-    // },
-    "gatsby-plugin-robots-txt",
-    "gatsby-plugin-sitemap",
-    // "gatsby-plugin-emotion",
-    {
-      resolve: `gatsby-plugin-s3`,
-      options: {
-        bucketName: "ahiravan.dev",
-        acl: null,
-        protocol: "https",
-        hostname: "www.ahiravan.dev",
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: process.env.GA_TRACKING_ID,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-breakpoints",
-      options: {
-        queries: {
-          xs: "(max-width: 320px)",
-          sm: "(max-width: 720px)",
-          md: "(max-width: 1024px)",
-          l: "(max-width: 1536px)",
-          portrait: "(orientation: portrait)",
-          posts_list_break: "(max-width: 550px)",
-        },
-      },
-    },
-    {
-      resolve: "gatsby-omni-font-loader",
-      options: {
-        // mode: "render-blocking",
-        // enableListener: false,
-        custom: [
-          // {
-          //   name: ["Iosevka Aile Web"],
-          //   file: "/fonts/webfont-iosevka-aile-10.1.1/iosevka-aile.css",
-          // },
-          // {
-          //   name: ["Iosevka Etoile Web"],
-          //   file: "/fonts/webfont-iosevka-etoile-10.1.1/iosevka-etoile.css",
-          // },
-          {
-            name: ["Iosevka Aile Optimized", "Iosevka Etoile Optimized"],
-            file: "/fonts/iosevka-optimized-10.1.1/font.min.css",
-          }
-        ],
-      },
-    },
-    {
-      // why this: https://github.com/jariz/gatsby-plugin-s3/blob/master/recipes/with-cloudfront.md
-      resolve: `gatsby-plugin-canonical-urls`,
-      options: {
-        siteUrl: "https://www.ahiravan.dev",
-      },
-    },
-    "gatsby-plugin-minify-html",
-    "gatsby-plugin-brotli",
-    `gatsby-plugin-perf-budgets`,
-    "gatsby-plugin-webpack-bundle-analyser-v2",
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-    // {
-    //   resolve: "@sentry/gatsby",
-    //   options: {
-    //     dsn: process.env.SENTRY_DSN
-    //   },
-    // },
-  ],
+  plugins: plugins,
 }
