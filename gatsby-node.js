076b9c41a9175926191a3ff2407c3282a002c428
Thulasi Ram <thulasi503@gmail.com>
2021-11-07 02:25:00 +0530

    fix: create graphql node for reading time

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,22 +6,72 @@
 
 // You can delete this file if you're not using it
 
+const readingTime = require("reading-time")
+const { createContentDigest } = require(`gatsby-core-utils`)
 
 exports.onCreateWebpackConfig = ({ actions }) => {
-    actions.setWebpackConfig({
-      devtool: 'eval-source-map',
-    })
-  }
+  actions.setWebpackConfig({
+    devtool: "eval-source-map",
+  })
+}
+
+exports.createSchemaCustomization = ({ actions }) => {
+  // BlogPost is defined in gatsby-theme-blog-core
+  const { createTypes } = actions
+  createTypes(`
+    type ReadingTime implements Node {
+      text: String!
+      blogPostID: ID!
+      blogPostTitle: String!
+      id: ID!
+    }
+    `)
+}
 
 const { createFilePath } = require(`gatsby-source-filesystem`)
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
+exports.onCreateNode = async ({
+  node,
+  getNode,
+  createNodeId,
+  actions,
+}) => {
+  const { createNode, createNodeField } = actions
+
   if (node.internal.type === `Mdx`) {
     const slug = createFilePath({ node, getNode, basePath: `pages` })
     createNodeField({
       node,
       name: `slug`,
       value: slug,
+    })
+
+    // somehow this works for Mdx nodes and not for MdxBlogPost nodes
+    const rTime = readingTime(node.internal.content)
+    createNodeField({
+      node,
+      name: `readingTime`,
+      value: rTime,
+    })
+  }
+
+  if (node.internal.type === `MdxBlogPost`) {
+
+    // couldnt do createNodeField for MdxBlogPost / BlogPost as those types doesnt include `fields`
+    // hence this reversed approach where we create a readingTime node which maps with blog post id
+    const pNode = getNode(node.parent)
+    const rTime = readingTime(pNode.internal.content)
+    const readingTimeId = createNodeId(`${node.id} >>> ReadingTime`)
+
+    await createNode({
+      id: readingTimeId,
+      blogPostID: node.id,
+      blogPostTitle: node.title,
+      text: rTime.text,
+      parent: node.id,
+      internal: {
+        type: "ReadingTime",
+        contentDigest: createContentDigest(rTime.text),
+      },
     })
   }
 }
